ec5ad5ca53c7f68cd37dcbb11af59c96351b9a46
KartikAmbupe <kartikambupe@gmail.com>
2025-04-01 14:05:32 +0530

    converted to TS

--- models/predict.py
+++ models/predict.py
@@ -6,8 +6,8 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
 # Load trained model & vectorizer
-model = joblib.load("models/best_fake_news_model.pkl")
-vectorizer = joblib.load("models/tfidf_vectorizer.pkl")
+model = joblib.load("models/trained/nb_news_classifier.pkl")
+vectorizer = joblib.load("models/trained/nb_tfidf_vectorizer.pkl")
 
 # Preprocessing function
 lemmatizer = WordNetLemmatizer()
@@ -30,24 +30,7 @@ def predict_news(news_text):
 
 # Test
 sample_text = """
-
-"""
-
-"""Real
-Israel launched a series of airstrikes across the Gaza Strip, targeting Hamas in what is described as the heaviest assault since a ceasefire was established in January. Gaza's Ministry of Health reported at least 44 fatalities from the strikes. The Israeli government stated that the attacks were a response to Hamas's refusal to release hostages and engage in ceasefire negotiations. Prime Minister Benjamin Netanyahu emphasized that Israel would intensify its military actions against Hamas.
-The strikes have caused significant destruction in Gaza, with explosions reported in multiple locations. Ambulances were seen rushing to Al Aqsa Hospital in central Gaza. The ceasefire, which had brought temporary relief, is now at risk of collapsing, potentially worsening the humanitarian crisis in the region.
-
-The government has introduced a new healthcare policy aimed at providing affordable medical services to all citizens. The policy includes subsidized health insurance and expanded access to rural areas
-
-
-"""
-
-"""Fake
-
-A viral article claims that NASA scientists have confirmed that ancient Indian temple structures have direct connections to extraterrestrial civilizations. The article alleges that NASA has conducted extensive research and found mysterious signals originating from Indian temples.
-
 An unidentified source has reported that an alien spacecraft secretly landed in California last night. Authorities have allegedly covered up the incident to prevent public panic.
-
 """
 
 print("Prediction:", predict_news(sample_text))
